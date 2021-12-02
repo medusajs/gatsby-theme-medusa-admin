b8baec17a21c88d65e43cd405c0c6b960a6c5a36
olivermrbl <oliver@mrbltech.com>
2021-12-02 19:10:10 +0100

    fix: prepare for theming

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,11 @@
 module.exports = {
   siteMetadata: {
     title: `Medusa Admin`,
-    description: `The best ecommerce software.`,
+    description: `Admin system for Medusa`,
     author: `@medusajs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    //{
-    //  resolve: `gatsby-source-filesystem`,
-    //  options: {
-    //    name: `images`,
-    //    path: `${__dirname}/src/images`,
-    //  },
-    //},
     {
       resolve: "gatsby-plugin-svgr",
       options: {
@@ -21,14 +14,6 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `medusa-admin`,
-        short_name: `medusa`,
-        icon: `src/images/logo.svg`,
-      },
-    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-create-client-paths`,
